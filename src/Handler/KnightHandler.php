a5cb94b041faffa2fa7f2a0443c4c4f105ddc7bc
Marwa <marwa.ounalli@celaneo.com>
2020-08-06 14:14:54 +0200

    handler and service

--- src/Handler/KnightHandler.php
+++ src/Handler/KnightHandler.php
@@ -3,8 +3,71 @@
 
 namespace App\Handler;
 
+use App\Entity\Knight;
+use App\Form\KnightType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormFactoryInterface;
 
-class KnightHandler
+
+class KnightHandler implements HandlerInterface
 {
+    private $entityManager;
+    private $formFactory;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        FormFactoryInterface $formFactory
+    ) {
+        $this->entityManager = $entityManager;
+        $this->formFactory = $formFactory;
+    }
+
+    /**
+     * Get a resource
+     *
+     * @param int $id
+     * @return Object
+     */
+    public function get($id)
+    {
+        $repository = $this->entityManager->getRepository(Knight::class);
+        return $repository->find($id);
+    }
+
+    /**
+     * Get a collection of resources
+     *
+     * @param int $limit
+     * @param int $offset
+     * @return array
+     */
+    public function all($limit, $offset)
+    {
+        $repository = $this->entityManager->getRepository(Knight::class);
+        return $repository->getKnights($limit, $offset);
+    }
+
+    /**
+     * Register a resource
+     *
+     * @param $resource
+     * @return mixed
+     */
+    public function post($resource)
+    {
+        $knight = new Knight();
+        $form = $this->formFactory->create(KnightType::class, $knight);
+        $form->submit($resource);
+
+        if ($form->isValid()) {
+            $this->entityManager->persist($knight);
+            $this->entityManager->flush();
+
+            return true;
+        }
+
+        return false;
+    }
 
 }
+
--- src/Services/Arena.php
+++ src/Services/Arena.php
@@ -1,10 +1,19 @@
 <?php
 
-
 namespace App\Services;
 
+use App\Entity\FightInterface;
 
 class Arena
 {
-
+    public function fight(FightInterface $Fighter1, FightInterface $Fighter2)
+    {
+        if ($Fighter1->calculatePowerLevel() > $Fighter2->calculatePowerLevel()) {
+            return 1;
+        } elseif ($Fighter2->calculatePowerLevel() > $Fighter1->calculatePowerLevel()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 }
